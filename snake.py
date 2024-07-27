e5ddd2d557ee70c74730ad812394241a71adb72d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-27 23:27:09 +0200

    Update snake.py
    game close possibility

--- snake.py
+++ snake.py
@@ -93,3 +93,7 @@ def gameLoop():
         snake_List.append(snake_Head)
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
